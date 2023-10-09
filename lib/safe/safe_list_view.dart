5945b67663586c9bc47a44901f51b3da93380a49
Simthembile-Shezi <s221517979@mandela.ac.za>
2023-10-09 17:11:54 +0200

    Fixing file picker

--- lib/safe/safe_list_view.dart
+++ lib/safe/safe_list_view.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_const_constructors_in_immutables, sort_child_properties_last
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_const_constructors_in_immutables, sort_child_properties_last, unused_catch_clause
 
 import 'dart:io';
 
@@ -6,6 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:vault/safe/file_item.dart';
+import 'package:file_picker/file_picker.dart';
 
 class SafeListView extends StatefulWidget {
   final String document;
@@ -47,7 +48,9 @@ class _SafeListView extends State<SafeListView> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => addFile(),
+        onPressed: () {
+          _openFileExplorer();
+        },
         child: Icon(Icons.add),
         backgroundColor: Colors.purpleAccent,
       ),
@@ -65,20 +68,50 @@ class _SafeListView extends State<SafeListView> {
     );
   }
 
-  void addFile() async {
-    File file = File("path.txt");
-    CollectionReference users = FirebaseFirestore.instance.collection('users');
-    DocumentReference<Object?> documentReference = await users.add({
-      "name": "file",
-      "date": Timestamp.now(),
-    });
-    _saveToStorage(documentReference.id, file);
+  Future<void> _openFileExplorer() async {
+    FilePickerResult? result;
+    if (widget.subCollection.compareTo("Pictures") == 0) {
+      result = await FilePicker.platform.pickFiles(type: FileType.image);
+    } else if (widget.subCollection.compareTo("Videos") == 0) {
+      result = await FilePicker.platform.pickFiles(type: FileType.video);
+    } else if (widget.subCollection.compareTo("Audios") == 0) {
+      result = await FilePicker.platform.pickFiles(type: FileType.audio);
+    } else if (widget.subCollection.compareTo("Documents") == 0) {
+      result = await FilePicker.platform.pickFiles(
+          type: FileType.custom,
+          allowedExtensions: ['pdf', 'docx', 'xlsx', 'txt']);
+    }
+
+    if (result != null) {
+      addFile(result);
+    }
   }
 
-  void _saveToStorage(String id, File file) {
+  void addFile(FilePickerResult result) async {
+    String filePath = result.files.single.path!;
+    String fileName = result.files.single.name;
+
+    CollectionReference collectionRef = FirebaseFirestore.instance
+        .collection('users')
+        .doc(widget.document)
+        .collection(widget.subCollection.toLowerCase());
+    DocumentReference<Object?> documentReference = await collectionRef.add({
+      "name": fileName,
+      "date": Timestamp.now(),
+    });
+    String extension = fileName.split('.').last;
+    _saveToStorage("${documentReference.id}.$extension", File(filePath));
+  }
+
+  void _saveToStorage(String fileName, File file) async {
     FirebaseStorage storage = FirebaseStorage.instance;
     String folder = widget.subCollection.toLowerCase();
-    String fileName = file.path.toString();
-    Reference ref = storage.ref().child('$folder/$fileName');
+    Reference storageReference = storage.ref().child('$folder/$fileName');
+    try {
+      UploadTask uploadTask = storageReference.putFile(file);
+      await uploadTask.whenComplete(() => {});
+    } on FirebaseException catch (e) {
+      // ...
+    }
   }
 }
